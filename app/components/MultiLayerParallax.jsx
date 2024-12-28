00873929b3fa6db80c1ff2ab1758008df19ddf44
mfolsom930 <86228082+mfolsom930@users.noreply.github.com>
2024-12-28 01:16:14 -0700

    why mobile spacing

--- app/components/MultiLayerParallax.jsx
+++ app/components/MultiLayerParallax.jsx
@@ -2,6 +2,9 @@ import { motion, useScroll, useTransform } from "motion/react";
 import React, { useRef } from "react";
 
 import styles from "./MultiLayerParallax.module.css";
+import Image from "next/image";
+
+import heroHome from "../../public/homeHeroHouse.png";
 
 export default function MultiLayerParallax() {
     const ref = useRef(null);
@@ -21,12 +24,14 @@ export default function MultiLayerParallax() {
             <motion.div
                 className="absolute inset-0 z-10"
                 style={{
-                    backgroundImage: `url(/homeHeroHouse.png)`,
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
+                    // backgroundImage: `url(/homeHeroHouse.png)`,
+                    // backgroundPosition: "center",
+                    // backgroundSize: "cover",
                     y: backgroundY,
                 }}
-            />
+            >
+                <Image src={heroHome} alt="House with lawn" objectFit="cover" fill/>
+            </motion.div>
 
         </div>
     );
