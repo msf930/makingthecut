b576c6839a807ee8ed71fc3da6a0b53ef771d501
mfolsom930 <86228082+mfolsom930@users.noreply.github.com>
2025-01-23 04:28:26 -0700

    add lenis scroll

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
+import {ReactLenis} from '@/app/utils/lenis';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,11 +21,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <ReactLenis root>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {children}
+          </body>
+        </ReactLenis>
     </html>
   );
 }
